a9f8d0926ff7e8c4d66d18e9327e18ae19f9c9f8
guofuyinan@gmail.com <guofuyinan@gmail.com>
2020-02-28 22:54:31 +0800

    use H2->Mysql

--- src/main/java/life/lemon/community/mapper/UserMapper.java
+++ src/main/java/life/lemon/community/mapper/UserMapper.java
@@ -1,4 +1,15 @@
 package life.lemon.community.mapper;
 
-public class UserMapper {
+import life.lemon.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface UserMapper {
+
+    @Insert({"insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})"})
+    int insert(User user);
+
+
 }
