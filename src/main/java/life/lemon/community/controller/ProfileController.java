2125ab8ac3a10c90094de23f58399d4232c7d9b0
guofuyinan@gmail.com <guofuyinan@gmail.com>
2020-03-02 23:54:12 +0800

    my questions

--- src/main/java/life/lemon/community/controller/ProfileController.java
+++ src/main/java/life/lemon/community/controller/ProfileController.java
@@ -1,43 +1,40 @@
 package life.lemon.community.controller;
 
 import life.lemon.community.dto.PaginationDTO;
-import life.lemon.community.dto.QuestionDTO;
-import life.lemon.community.mapper.QuestionMapper;
 import life.lemon.community.mapper.UserMapper;
-import life.lemon.community.model.Question;
 import life.lemon.community.model.User;
 import life.lemon.community.service.QusetionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
-public class IndexController {
+public class ProfileController {
 
     @Autowired
     private UserMapper userMapper;
-
     @Autowired
     private QusetionService qusetionService;
 
-    @GetMapping("/")
-    public String index(HttpServletRequest request,
-                        Model model,
-                        @RequestParam(name = "page",defaultValue = "1") Integer page,
-                        @RequestParam(name = "size",defaultValue = "5") Integer size){
-
+    @GetMapping("/profile/{action}")
+    public String profile(HttpServletRequest request,
+                          @PathVariable(name = "action") String action,
+                          Model model,
+                          @RequestParam(name = "page",defaultValue = "1") Integer page,
+                          @RequestParam(name = "size",defaultValue = "5") Integer size){
+        User user = null;
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
                 if (cookie.getName().equals("token")) {
                     String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
+                    user = userMapper.findByToken(token);
                     if (user != null) {
                         request.getSession().setAttribute("user", user);
                     }
@@ -45,10 +42,21 @@ public class IndexController {
                 }
             }
         }
+        if (user == null){
+            return "redirect:/";
+        }
+        if ("questions".equals(action)){
+            model.addAttribute("section","questions");
+            model.addAttribute("sectionName","我的提问");
+        }else if ("replies".equals(action)){
+            model.addAttribute("section","replies");
+            model.addAttribute("sectionName","最新回复");
 
-        PaginationDTO pagination = qusetionService.list(page,size);
-        model.addAttribute("pagination", pagination);
-        return "index";
+        }
+        PaginationDTO paginationDTO = qusetionService.list(user.getId(), page, size);
+        model.addAttribute("pagination", paginationDTO);
 
+        return "profile";
     }
+
 }
--- src/main/java/life/lemon/community/service/QusetionService.java
+++ src/main/java/life/lemon/community/service/QusetionService.java
@@ -48,4 +48,33 @@ public class QusetionService {
         paginationDTO.setQuestions(questionDTOList);
         return paginationDTO;
     }
+
+    public PaginationDTO list(Integer userId, Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+
+
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > paginationDTO.getTotalPage()) {
+            page = paginationDTO.getTotalPage();
+        }
+
+        Integer offset = (page-1) * size;
+        List<Question> questions = questionMapper.listByUserId(userId,offset,size);
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+        for (Question question : questions) {
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question, questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        paginationDTO.setQuestions(questionDTOList);
+        return paginationDTO;
+
+    }
 }
