cf6abaa6e5dff526e6582e4d2ee4379547adf396
guofuyinan@gmail.com <guofuyinan@gmail.com>
2020-03-04 11:04:29 +0800

    Fix ID  BUG

---
+++ src/main/java/life/lemon/community/service/UserService.java
@@ -0,0 +1,30 @@
+package life.lemon.community.service;
+
+import life.lemon.community.mapper.UserMapper;
+import life.lemon.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    public void createOrUpdate(User user) {
+        User dbUser = userMapper.findByAccountId(user.getAccountId());
+        if (dbUser == null){
+            //插入数据
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+
+        }else {
+            //更新数据
+            dbUser.setGmtModified(System.currentTimeMillis());
+            dbUser.setAvatarUrl(user.getAvatarUrl());
+            dbUser.setName(user.getName());
+            dbUser.setToken(user.getToken());
+            userMapper.update(dbUser);
+        }
+    }
+}
